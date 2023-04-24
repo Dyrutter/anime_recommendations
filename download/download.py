450fbea82cd282c7f1c2349a914ae6a8a42fe329
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-04-24 13:52:40 -0500

    Update download.py

--- download/download.py
+++ download/download.py
@@ -1,67 +1,224 @@
-name: download
-conda_env: conda.yml
+import argparse
+import logging
+import pathlib
+import wandb
+import requests
+import tempfile
+import os
+import pandas as pd
+from distutils.util import strtobool
 
-entry_points:
-  main:
-    parameters:
-      stats_url:
-        description: URL of the stats data file to download
-        type: uri
-      stats_artifact:
-        description: Name for the W&B stats data artifact that will be created
-        type: str
-      stats_artifact_type:
-        description: Type of the stats data artifact to create
-        type: str
-        default: raw_data
-      stats_artifact_description:
-        description: Description for the stats data artifact
-        type: str
-      synopses_url:
-        description: URL of the synopses data file to download
-        type: uri
-      synopses_artifact:
-        description: Name for the W&B synopses data artifact that will be created
-        type: str
-      synopses_artifact_type:
-        description: Type of the synopses data artifact to create
-        type: str
-        default: raw_data
-      synopses_artifact_description:
-        description: Description for the synopses data artifact
-        type: str
-      all_anime_url:
-        description: URL of the all_anime data file to download
-        type: uri
-      all_anime_artifact:
-        description: Name for the W&B all_anime data artifact that will be created
-        type: str
-      all_anime_type:
-        description: Type of the all_anime data artifact to create
-        type: str
-        default: raw_data
-      all_anime_description:
-        description: Description for the all_anime data artifact
-        type: str
-      save_raw_locally:
-        description: Boolean of whether or not to save raw data to local machine
-        type: str
-      project_name:
-        description: Name of wandb project
-        type: str
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
-    command: >-
-      python download.py --stats_url {stats_url} \
-                              --stats_artifact {stats_artifact} \
-                              --stats_artifact_type {stats_artifact_type} \
-                              --stats_artifact_description {stats_artifact_description} \
-                              --synopses_url {synopses_url} \
-                              --synopses_artifact {synopses_artifact} \
-                              --synopses_artifact_type {synopses_artifact_type} \
-                              --synopses_artifact_description {synopses_artifact_description} \
-                              --all_anime_url {all_anime_url} \
-                              --save_raw_locally {save_raw_locally} \
-                              --all_anime_artifact {all_anime_artifact} \
-                              --all_anime_type {all_anime_type} \
-                              --project_name {project_name} \
-                              --all_anime_description {all_anime_description}
+
+def go(args):
+    # Derive the base name of the data file from the URL
+    stats_basename = pathlib.Path(
+        args.stats_url).name.split("?")[0].split("#")[0]
+    synopses_basename = pathlib.Path(
+        args.synopses_url).name.split("?")[0].split("#")[0]
+    all_anime_basename = pathlib.Path(
+        args.all_anime_url).name.split("?")[0].split("#")[0]
+
+    logger.info(f"Downloading {args.stats_artifact}")
+    logger.info(f"Downloading {args.synopses_artifact}")
+    logger.info(f"Downloading {args.all_anime_artifact}")
+
+    stats_file = os.path.join(os.getcwd(), f"{args.stats_artifact}")
+    synopses_file = os.path.join(os.getcwd(), f"{args.synopses_artifact}")
+    all_anime_file = os.path.join(os.getcwd(), f"{args.all_anime_artifact}")
+
+    # Create stats artifact
+    # Download file, streaming so we can download files larger than
+    # the available memory. Named temporary file gets destroyed at the end so
+    # nothing is left behind and the file gets removed even in case of errors
+    with tempfile.NamedTemporaryFile(mode='wb+') as fp:
+        logger.info("Creating run for downloading stats file")
+        with wandb.init(
+                job_type="download_data",
+                project=args.project_name) as run:
+            # Download the file streaming and write to open temp file
+            with requests.get(args.stats_url, stream=True) as r:
+                for chunk in r.iter_content(chunk_size=8192):
+                    fp.write(chunk)
+            fp.flush()  # ensure file written to disk before uploading to w&B
+            logger.info("Creating stats artifact")
+            artifact = wandb.Artifact(
+                name=args.stats_artifact,
+                type=args.stats_artifact_type,
+                description=args.stats_artifact_description,
+                metadata={'stats_url': args.stats_url})
+            artifact.add_file(fp.name, name=stats_basename)
+            logger.info("Logging stats artifact")
+            run.log_artifact(artifact)
+            artifact.wait()
+
+    # Create synopses artifact
+    with tempfile.NamedTemporaryFile(mode='wb+') as fp:
+        logger.info("Creating run for downloading synopses data")
+        with wandb.init(
+                job_type="download_data",
+                project=args.project_name) as run:
+            with requests.get(args.synopses_url, stream=True) as r:
+                for chunk in r.iter_content(chunk_size=8192):
+                    fp.write(chunk)
+            fp.flush()
+            logger.info("Creating synopses artifact")
+            artifact = wandb.Artifact(
+                name=args.synopses_artifact,
+                type=args.synopses_artifact_type,
+                description=args.synopses_artifact_description,
+                metadata={'synopses_url': args.synopses_url})
+            artifact.add_file(fp.name, name=synopses_basename)
+            logger.info("Logging synopses artifact")
+            run.log_artifact(artifact)
+            artifact.wait()
+
+    # Create all_anime artifact
+    with tempfile.NamedTemporaryFile(mode='wb+') as fp:
+        logger.info("Creating run for downloading all_anime data")
+        with wandb.init(
+                job_type="download_data",
+                project=args.project_name) as run:
+            with requests.get(args.all_anime_url, stream=True) as r:
+                for chunk in r.iter_content(chunk_size=8192):
+                    fp.write(chunk)
+            fp.flush()
+            logger.info("Creating all_anime artifact")
+            artifact = wandb.Artifact(
+                name=args.all_anime_artifact,
+                type=args.all_anime_type,
+                description=args.all_anime_description,
+                metadata={'all_anime_url': args.all_anime_url})
+            artifact.add_file(fp.name, name=all_anime_basename)
+            logger.info("Logging all_anime artifact")
+            run.log_artifact(artifact)
+            artifact.wait()
+
+    # Save files to local system if desired
+    if args.save_raw_locally is True:
+        with wandb.init(
+                job_type='download_data',
+                project=args.project_name) as run:
+            stats_artifact = run.use_artifact(args.stats_artifact)
+            stats_artifact_path = stats_artifact.file()
+            stats_df = pd.read_parquet(stats_artifact_path, low_memory=False)
+            stats_df.to_parquet(stats_file, index=False)
+
+            synopses_artifact = run.use_artifact(args.synopses_artifact)
+            synopses_artifact_path = synopses_artifact.file()
+            synopses_df = pd.read_csv(synopses_artifact_path, low_memory=False)
+            synopses_df.to_csv(synopses_file, index=False)
+
+            all_anime_artifact = run.use_artifact(args.all_anime_artifact)
+            all_anime_path = all_anime_artifact.file()
+            all_anime_df = pd.read_csv(all_anime_path, low_memory=False)
+            all_anime_df.to_csv(all_anime_file, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download files and upload them as artifacts to W&B",
+        fromfile_prefix_chars="@")
+
+    parser.add_argument(
+        "--stats_url",
+        type=str,
+        help="File URL",
+        required=True
+    )
+
+    parser.add_argument(
+        "--project_name",
+        type=str,
+        help="Name of wandb project",
+        required=True
+    )
+
+    parser.add_argument(
+        "--stats_artifact",
+        type=str,
+        help="Name for artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--stats_artifact_type",
+        type=str,
+        help="Type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--stats_artifact_description",
+        type=str,
+        help="Description for the new artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--synopses_url",
+        type=str,
+        help="File URL",
+        required=True
+    )
+
+    parser.add_argument(
+        "--synopses_artifact",
+        type=str,
+        help="Name for artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--synopses_artifact_type",
+        type=str,
+        help="Type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--synopses_artifact_description",
+        type=str,
+        help="Description for the new artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--all_anime_url",
+        type=str,
+        help="File URL",
+        required=True
+    )
+
+    parser.add_argument(
+        "--all_anime_artifact",
+        type=str,
+        help="Name for artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--all_anime_type",
+        type=str,
+        help="Type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--all_anime_description",
+        type=str,
+        help="Description for the new artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--save_raw_locally",
+        type=lambda x: bool(strtobool(x)),
+        help='Choose whether or not to save clean data frame to local file',
+        required=True
+    )
+
+    args = parser.parse_args()
+    go(args)
