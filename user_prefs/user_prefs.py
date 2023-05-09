97a17c503d01e2f92d04233cb9d30cf1e2551091
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-05-09 14:22:44 -0500

    Add files via upload

---
+++ user_prefs/user_prefs.py
@@ -0,0 +1,75 @@
+import argparse
+import logging
+# import os
+import wandb
+import string
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from distutils.util import strtobool
+import random
+import ast
+import re
+from wordcloud import WordCloud
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+logging.basicConfig(
+    filename='./user_prefs.log',  # Path to log file
+    level=logging.INFO,  # Log info, warnings, errors, and critical errors
+    filemode='a',  # Create log file if one doesn't already exist and add
+    format='%(asctime)s-%(name)s - %(levelname)s - %(message)s',
+    datefmt='%d %b %Y %H:%M:%S %Z',  # Format date
+    force=True)
+logger = logging.getLogger()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Get user preferences",
+        fromfile_prefix_chars="@",
+    )
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        help="Wandb artifact with .h5 file of neural network",
+        required=True
+    )
+
+    parser.add_argument(
+        "--main_df",
+        type=str,
+        help="Main prerpocessed data frame",
+        required=True
+    )
+    parser.add_argument(
+        "--project_name",
+        type=str,
+        help="Name of wandb project",
+        required=True
+    )
+
+    parser.add_argument(
+        "--anime_df",
+        type=str,
+        help="anime df",
+        required=True
+    )
+
+    parser.add_argument(
+        "--user_query",
+        type=str,
+        help="input user id to query",
+        required=True
+    )
+
+    parser.add_argument(
+        "--random_user",
+        type=lambda x: bool(strtobool(x)),
+        help="Decide whether or not to use a random user id",
+        required=True
+    )
+
+    args = parser.parse_args()
+    go(args)
