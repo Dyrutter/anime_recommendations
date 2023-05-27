d2444b0466dc55573b1fa4dd99daeeaa0ce6e557
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-05-27 11:26:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -140,17 +140,23 @@ def go(config: DictConfig):
             os.path.join(root_path, "similar_users"),
             entry_point="main",
             parameters={
-                "weights": config["nn_arts"]["main_weights_art"],
-                "history": config["nn_arts"]["main_history"],
+                "anime_df": config["data"]["all_anime_artifact_latest"],
+                "anime_df_type": config["data"]["all_anime_type"],
                 "model": config["nn_arts"]["main_model"],
+                "model_type": config["nn_arts"]["model_type"],
                 "project_name": config["main"]["project_name"],
                 "main_df": config["data"]["preprocessed_artifact_latest"],
-                "user_query": config["similarity"]["user_query"],
+                "main_df_type": config["data"]["preprocessed_artifact_type"],
+                "user_query": config["users"]["user_query"],
                 "id_query_number": config["similarity"]["id_query_number"],
                 "max_ratings": config["similarity"]["max_ratings"],
                 "random_user": config["similarity"]["random_user"],
                 "num_faves": config["users"]["num_faves"],
-                "TV_only": config["users"]["TV_only"]})
+                "TV_only": config["users"]["TV_only"],
+                "sim_users_fn": config["users"]["sim_users_fn"],
+                "sim_users_type": config["users"]["sim_users_type"],
+                "ID_fn": config["users"]["ID_fn"],
+                "ID_type": config["users"]["ID_type"]})
 
     if "user_prefs" in steps_to_execute:
         _ = mlflow.run(
@@ -161,7 +167,7 @@ def go(config: DictConfig):
                 "main_df": config["data"]["preprocessed_artifact_latest"],
                 "project_name": config["main"]["project_name"],
                 "anime_df": config["data"]["all_anime_artifact_latest"],
-                "user_query": config["similarity"]["user_query"],
+                "user_query": config["user"]["user_query"],
                 "random_user": config["similarity"]["random_user"],
                 "favorite_percentile": config["users"]["favorite_percentile"],
                 "show_clouds": config["users"]["show_clouds"],
@@ -171,7 +177,15 @@ def go(config: DictConfig):
                 "cloud_height": config["users"]["cloud_height"],
                 "prefs_csv": config["users"]["prefs_csv"],
                 "interval": config["main"]["interval"],
-                "save_faves": config["users"]["save_faves"]})
+                "save_faves": config["users"]["save_faves"],
+                "flow_user": config["users"]["flow_user"],
+                "from_flow": config["users"]["from_flow"],
+                "use_local_user": config["users"]["use_local_user"],
+                "main_df_type": config["data"]["preprocessed_artifact_type"],
+                "anime_df_type": config["data"]["all_anime_type"],
+                "ID_type": config["users"]["ID_type"],
+                "cloud_type": config["users"]["cloud_type"],
+                "fave_art_type": config["users"]["fave_art_type"]})
 
     if "user_recs" in steps_to_execute:
         _ = mlflow.run(
@@ -181,7 +195,7 @@ def go(config: DictConfig):
                 "main_df": config["data"]["preprocessed_artifact_latest"],
                 "project_name": config["main"]["project_name"],
                 "anime_df": config["data"]["all_anime_artifact_latest"],
-                "user_query": config["similarity"]["user_query"],
+                "user_query": config["user"]["user_query"],
                 "use_random_user": config["users"]["use_random_user"],
                 "user_recs_fn": config["users"]["user_recs_fn"],
                 "save_user_recs": config["users"]["save_user_recs"],
@@ -212,7 +226,7 @@ def go(config: DictConfig):
                 "sypnopsis_type": config["data"]["synopses_artifact_type"],
                 "model": config["nn_arts"]["main_model"],
                 "model_type": config["nn_arts"]["model_type"],
-                "user_query": config["similarity"]["user_query"],
+                "user_query": config["user"]["user_query"],
                 "random_user": config["similarity"]["random_user"],
                 "model_recs_fn": config["model_recs"]["model_recs_fn"],
                 "save_model_recs": config["model_recs"]["save_model_recs"],
